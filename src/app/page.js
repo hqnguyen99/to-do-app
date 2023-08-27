fccaa6f83c52c836251344481a94dd76fcbb5276
John Nguyen <hqnguyen@sfu.ca>
2023-08-27 09:47:41 -0700

    Finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,111 @@
+'use client';
+import { useState,useRef,useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import Image from 'next/image'
 import styles from './page.module.css'
+import Todo from '@/components/Todo';
+import Form from '@/components/Form';
+import FilterButton from '@/components/FilterButton';
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Home() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [filter,setFilter]= useState("All");
+  const listHeadingRef= useRef(null);
+  function toggleTaskCompleted(id) {
+    const updateTasks= tasks.map((task)=>{
+      if(task.id == id) {
+        return {...task, completed: !task.completed}
+      };
+      return task;
+    })
+    setTasks(updateTasks);
+  }
+
+  function deleteTask(id){
+    const updateTasks = tasks.filter((task)=>{
+      return task.id !=id;
+    })
+    setTasks(updateTasks);
+  }
+  function editTask(id,newName){
+    const updateTasks= tasks.map((task)=>{
+      console.log(id);
+      if( id == task.id){
+        return {...task,name: newName};
+      }
+      return task;
+    })
+    setTasks(updateTasks);
+  }
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  
+  const filterList= FILTER_NAMES.map((name)=> (
+    <FilterButton 
+    key={name} 
+    name= {name}
+    isPressed = {name == filter}
+    handleFilter= {setFilter}/>
+  ))
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(()=>{
+    if(prevTaskLength == tasks.lenght+1){
+      listHeadingRef.current.focus();
+    }
+  },[prevTaskLength,tasks.length])
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        {filterList}
+
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <h2 id="list-heading" tabIndex="-1" ref= {listHeadingRef}>{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
 }
+const TASKS = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Study", completed: false },
+]
